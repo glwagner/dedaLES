cff666ef503caeaf21e2ba74697b9c7f9e08f300
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2018-11-28 17:38:19 -0500

    Reorganizes files

--- testfish.py
+++ testfish.py
@@ -1,7 +1,8 @@
+import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import fishalus
 
-model = fishalus.RayleighBernard()
+model = fishalus.DeepConvectionModel()
 model.set_random_ic()
 model.run()
