49843423a7884e74f2302dbc195fb0cbd61e7b0e
Keaton J. Burns <keaton.burns@gmail.com>
2018-11-30 14:33:43 -0500

    Add slurm example file for engaging

---
+++ sandbox/slurm_engaging.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Partition             Nodes   S-C-T   Timelimit
+# ---------             -----   -----   ---------
+# sched_mit_hill        (32)    2-8-1   12:00:00
+# sched_any_quicktest   2       2-8-1   00:15:00
+# newnodes              (32)    2-10-1  12:00:00
+
+# Job
+#SBATCH --partition=sched_mit_hill
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=16
+#SBATCH --time=12:00:00
+
+# Streams
+#SBATCH --output=job_%j.out
+#SBATCH --error=job_%j.err
+
+# Content
+mpiexec echo "test"
+
