0400e8a2a8a81d339343b797988ab26e3a1b3193
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2018-12-12 12:03:24 -0500

    Printing for MPI jobs

--- dedaLES/utils.py
+++ dedaLES/utils.py
@@ -1,9 +1,12 @@
 import numpy as np
 from mpi4py import MPI
-from dedalus import public as de
 
 def random_noise(domain):
     gshape = domain.dist.grid_layout.global_shape(scales=1)
     slices = domain.dist.grid_layout.slices(scales=1)
     rand = np.random.RandomState(seed=23)
     return rand.standard_normal(gshape)[slices]
+
+def mpiprint(msg):
+    if MPI.COMM_WORLD.Get_rank() is 0:
+        print(msg)
