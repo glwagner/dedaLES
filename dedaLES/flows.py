a582cd28282cc8f886b6d689b26ce17b47252f01
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2018-12-12 12:03:04 -0500

    Generalizing and functionalizing, plus cleanup

--- dedaLES/flows.py
+++ dedaLES/flows.py
@@ -1,9 +1,8 @@
 import numpy as np
 import time, logging
-from mpi4py import MPI
 
+from mpi4py import MPI
 from dedalus import public as de
-from dedalus.extras import flow_tools
 
 logger = logging.getLogger(__name__)
 
@@ -12,17 +11,75 @@ logger = logging.getLogger(__name__)
 minute = 60
 hour = 60*minute
 
-def addparams(problem, **params):
-    for k, v in params.items():
-        problem.parameters[k] = v
+def add_parameters(problem, **params):
+    """
+    Add parameters to a dedalus problem programmatically.
+    """
+    for name, value in params.items():
+        problem.parameters[name] = value
+
+def add_derivative_substitutions(problem, dim, vars=[]):
+    for var in vars:
+        problem.substitutions[var+dim] = f"d{dim}({var})"
+
+def add_closure(problem, closure, tracers=[]):
+    """
+    Add closure substitutions and equations to the problem.
+    """
+    if closure is None:
+        # No parameterized subgrid fluxes
+        problem.substitutions['Fx_sgs'] = "0"
+        problem.substitutions['Fy_sgs'] = "0"
+        problem.substitutions['Fz_sgs'] = "0"
+        problem.substitutions['Fb_sgs'] = "0"
+    else:
+        closure.substitutions(problem, tracers=tracers)
+
+        
+def bind_parameters(obj, **params):
+    """
+    Bind the name, value pairs in `params` as members of the class `obj`.
+    """
+    for name, value in params.items():
+        setattr(obj, name, value)
+
 
 class ChannelFlow():
-    def __init__(self):
-        pass
+    """
+    A parent class for fluid models in channels.
+    """
+    def __init__(self, nx, ny, nz, Lx, Ly, Lz, xleft, yleft, zbottom, incompressible=True):
+
+        # Default origin: (-Lx/2, -Ly/2, -Lz)
+        if xleft is None: xleft = -Lx/2
+        if yleft is None: yleft = -Ly/2
+        if zbottom is None: zbottom = 0.0
+
+        xright = xleft + Lx
+        yright = yleft + Ly
+        ztop = zbottom + Lz
+
+        self.xlimits = (xleft, xright)
+        self.ylimits = (yleft, yright)
+        self.zlimits = (zbottom, ztop)
+
+        self.nx = nx
+        self.ny = ny
+        self.nz = nz
+
+        self.Lx = Lx
+        self.Ly = Ly
+        self.Lz = Lz
+
+        self.incompressible = incompressible
 
     # Boundary condition API.
     def set_nopenetration_bc_top(self):
-        self.problem.add_bc("right(w) = 0", condition="(nx != 0) or (ny != 0)")
+        # Forbid a domain-averaged vertical velocity.
+        if self.incompressible:
+            self.problem.add_bc("right(w) = 0", condition="(nx != 0) or (ny != 0)") 
+        else:
+            self.problem.add_bc("right(w) = 0")
 
     def set_nopenetration_bc_bottom(self):
         self.problem.add_bc("left(w) = 0")
@@ -51,26 +108,26 @@ class ChannelFlow():
         self.problem.add_bc("left(u) = {}".format(u))
         self.problem.add_bc("left(v) = {}".format(v))
 
-    def set_tracer_gradient_bc_bottom(self, tracers, gradient=0):
+    def set_tracer_gradient_bc_bottom(self, tracers=[], gradient=0):
         if isinstance(tracers, str):
             self.problem.add_bc("left({}z) = {}".format(tracers, gradient))
         else:
             for tracer in tracers:
                 self.problem.add_bc("left({}z) = {}".format(tracer, gradient))
 
-    def set_tracer_gradient_bc_top(self, tracers, gradient=0):
+    def set_tracer_gradient_bc_top(self, tracers=[], gradient=0):
         if isinstance(tracers, str):
             self.problem.add_bc("right({}z) = {}".format(tracers, gradient))
         else:
             for tracer in tracers:
                 self.problem.add_bc("right({}z) = {}".format(tracer, gradient))
 
-    def set_tracer_gradient_bc(self, tracers, *walls, gradient=0):
+    def set_tracer_gradient_bc(self, tracers=[], *walls, gradient=0):
         for wall in walls:
             method = getattr(self, "set_tracer_gradient_bc_%s" %wall)
             method(tracers, gradient=gradient)
 
-    def set_tracer_noflux_bc(self, tracers, *walls):
+    def set_tracer_noflux_bc(self, *walls, tracers=[]):
         self.set_tracer_gradient_bc(tracers, *walls, gradient=0)
 
     def set_bc(self, bctype, *walls, **kwargs):
@@ -105,7 +162,7 @@ class ChannelFlow():
             method = getattr(self, 'set_%s_bc_%s' %(bctype, wall))
             method(**kwargs)
 
-    def set_tracer_bc(self, bctype, tracers, *walls, **kwargs):
+    def set_tracer_bc(self, bctype, *walls, tracers=[], **kwargs):
         """
         Set boundary conditions for a tracer in a channel model.
 
@@ -154,6 +211,12 @@ class ChannelFlow():
         for name, value in fields.items():
             self.set_field(name, value)
 
+    def add_log_task(self, name, task):
+        try:
+            self.log_tasks[name] = task
+        except AttributeError:
+            self.log_tasks = {name: task}
+
     def build_solver(self, timestepper='RK443'):
         """
         Build a dedalus solver for the model with `timestepper`.
@@ -175,12 +238,6 @@ class ChannelFlow():
 
     def time_to_log(self, log_cadence):
         return (self.solver.iteration-1) % log_cadence == 0
-
-    def init_flow(self, cadence=10):
-        self.flow = flow_tools.GlobalFlowProperty(self.solver, cadence=cadence)
-
-    def add_flow_KE(self):
-        self.flow.add_property("0.5*(u*u + v*v + w*w)", name="KE")
 
     """
         run(self, **kwargs)
@@ -249,105 +306,87 @@ class BoussinesqChannelFlow(ChannelFlow):
         **params : additional parameters to be added to the dedalus problem.
     """
     def __init__(self,
-        nx = 64,
-        ny = 64,
-        nz = 64,
-        Lx = 1.0,        # [m]
-        Ly = 1.0,        # [m]
-        Lz = 1.0,        # [m]
-        f = 0.0,         # Coriolis parameter [s⁻¹]
-        κ = 1.43e-7,     # Diffusivity [m²/s]
-        ν = 1e-6,        # Kinematic viscosity [m²/s]
+        nx  = 32,
+        ny  = 32,
+        nz  = 32,
+        Lx  = 1.0,       # [m]
+        Ly  = 1.0,       # [m]
+        Lz  = 1.0,       # [m]
+        f   = 0.0,       # Coriolis parameter [s⁻¹]
+        κ   = 1.43e-7,   # Diffusivity [m²/s]
+        ν   = 1.05e-6,   # Kinematic viscosity [m²/s]
         Nsq = 0.0,       # Background buoyancy gradient [s⁻²]
         closure = None,  # Subgrid closure
+        xleft   = None,  # x-origin of domain (default: -Lx/2)
+        yleft   = None,  # y-origin of domain (default: -Ly/2)
+        zbottom = None,  # z-coordinate of channel bottom (default is -Lz)
         **params         # Additional parameters to be added to dedalus problem
         ):
 
-        # Create bases and domain
-        xbasis = de.Fourier('x', nx, interval=(-Lx/2, Lx/2), dealias=3/2)
-        ybasis = de.Fourier('y', ny, interval=(-Ly/2, Ly/2), dealias=3/2)
-        zbasis = de.Chebyshev('z', nz, interval=(-Lz, 0), dealias=3/2)
-        domain = de.Domain([xbasis, ybasis, zbasis], grid_dtype=np.float64)
+        ChannelFlow.__init__(self, nx, ny, nz, Lx, Ly, Lz, xleft, yleft, zbottom)
 
-        self.xbasis = xbasis
-        self.ybasis = ybasis
-        self.zbasis = zbasis
-        self.domain = domain
+        # Create bases and domain
+        self.xbasis = xbasis = de.Fourier('x', nx, interval=self.xlimits, dealias=3/2)
+        self.ybasis = ybasis = de.Fourier('y', ny, interval=self.ylimits, dealias=3/2)
+        self.zbasis = zbasis = de.Chebyshev('z', nz, interval=self.zlimits, dealias=3/2)
+        self.domain = domain = de.Domain([xbasis, ybasis, zbasis], grid_dtype=np.float64)
 
         self.x = domain.grid(0)
         self.y = domain.grid(1)
         self.z = domain.grid(2)
 
-        self.Lx = Lx
-        self.Ly = Ly
-        self.Lz = Lz
-
-        self.f = f
-        self.κ = κ
-        self.ν = ν
-
         # 3D rotating Boussinesq hydrodynamics
         self.problem = problem = de.IVP(domain, variables=['p', 'b', 'u', 'v', 'w', 'bz', 'uz', 'vz', 'wz'], time='t')
 
         # Add additional parameters passed to constructor to the dedalus Problem.
-        addparams(problem, κ=κ, ν=ν, f=f, Nsq=Nsq, **params)
-        for name, value in params.items():
-            setattr(self, name, value)
+        add_parameters(problem, κ=κ, ν=ν, f=f, Nsq=Nsq, **params)
+        bind_parameters(self, f=f, κ=κ, ν=ν, **params)
 
         # First-order substitutions
-        problem.substitutions['ux'] = "dx(u)"
-        problem.substitutions['vx'] = "dx(v)"
-        problem.substitutions['wx'] = "dx(w)"
-        problem.substitutions['bx'] = "dx(b)"
-        problem.substitutions['uy'] = "dy(u)"
-        problem.substitutions['vy'] = "dy(v)"
-        problem.substitutions['wy'] = "dy(w)"
-        problem.substitutions['by'] = "dy(b)"
+        add_derivative_substitutions(problem, 'x', vars=['u', 'v', 'w', 'b'])
+        add_derivative_substitutions(problem, 'y', vars=['u', 'v', 'w', 'b'])
 
-        # Closure substitutions
-        if closure is None:
-            # No parameterized subgrid fluxes
-            problem.substitutions['Fx_sgs'] = "0"
-            problem.substitutions['Fy_sgs'] = "0"
-            problem.substitutions['Fz_sgs'] = "0"
-            problem.substitutions['Fb_sgs'] = "0"
-        else:
-            closure.substitutions(problem, tracers=["b"])
-
+        # LES Closure
+        add_closure(problem, closure, tracers=['b'])
+                   
         # Momentum equations
-        problem.add_equation("dt(u) - f*v   - ν*(dx(ux) + dy(uy) + dz(uz)) + dx(p) = - u*ux - v*uy - w*uz + Fx_sgs")
-        problem.add_equation("dt(v) + f*u   - ν*(dx(vx) + dy(vy) + dz(vz)) + dy(p) = - u*vx - v*vy - w*vz + Fy_sgs")
-        problem.add_equation("dt(w) - b     - ν*(dx(wx) + dy(wy) + dz(wz)) + dz(p) = - u*wx - v*wy - w*wz + Fz_sgs")
-        problem.add_equation("dt(b) + Nsq*w - κ*(dx(bx) + dy(by) + dz(bz))         = - u*bx - v*by - w*bz + Fb_sgs")
+        problem.add_equation("dt(u) - ν*(dx(ux) + dy(uy) + dz(uz)) + dx(p) - f*v = - u*ux - v*uy - w*uz + Fx_sgs")
+        problem.add_equation("dt(v) - ν*(dx(vx) + dy(vy) + dz(vz)) + dy(p) + f*u = - u*vx - v*vy - w*vz + Fy_sgs")
+        problem.add_equation("dt(w) - ν*(dx(wx) + dy(wy) + dz(wz)) + dz(p) - b   = - u*wx - v*wy - w*wz + Fz_sgs")
+
+        # Buoyancy equation
+        problem.add_equation("dt(b) - κ*(dx(bx) + dy(by) + dz(bz)) + Nsq*w       = - u*bx - v*by - w*bz + Fb_sgs")
+
+        # Continuity equation
         problem.add_equation("ux + vy + wz = 0")
 
+        # First-order equivalencies
         problem.add_equation("bz - dz(b) = 0")
         problem.add_equation("uz - dz(u) = 0")
         problem.add_equation("vz - dz(v) = 0")
         problem.add_equation("wz - dz(w) = 0")
 
+        # Pressure gauge condition
         problem.add_bc("right(p) = 0", condition="(nx == 0) and (ny == 0)")
-
-        self.log_tasks = {}
         
-    def add_log_task(self, name, task):
-        self.log_tasks[name] = task
-
-    def add_flow_variance(self):
-        self.flow.add_property("0.5*b*b", name="variance")
-
     def set_noflux_bc_top(self):
-        ChannelFlow.set_tracer_noflux_bc(self, "b", "top")
+        """
+        A convenience method to set a no flux condition on buoyancy
+        on the top wall of the channel.
+        """
+        ChannelFlow.set_tracer_noflux_bc(self, "top", tracers="b")
 
     def set_noflux_bc_bottom(self):
-        ChannelFlow.set_tracer_noflux_bc(self, "b", "bottom")
-
-    def set_default_bcs(self):
-        self.set_bc("nopenetration", "top", "bottom")
-        self.set_bc("noslip", "top", "bottom")
-        self.set_bc("noflux", "top", "bottom")
+        """
+        A convenience method to set a no flux condition on buoyancy
+        on the bottom wall of the channel.
+        """
+        ChannelFlow.set_tracer_noflux_bc(self, "bottom", tracers="b")
 
     def build_solver(self, timestepper='RK443'):
+        """
+        Build a BoussinesqChanelFlow solver.
+        """
 
         ChannelFlow.build_solver(self, timestepper=timestepper)
 
@@ -361,12 +400,11 @@ class BoussinesqChannelFlow(ChannelFlow):
         self.wz = self.solver.state['wz']
         self.bz = self.solver.state['bz']
 
-        self.init_flow()
-        self.add_flow_KE()
-        self.add_flow_variance()
-
 
     def log(self, logger, dt):
+        """
+        Print messages.
+        """
         logger.info('Iteration: %i, Time: %e, dt: %e' %(self.solver.iteration, self.solver.sim_time, dt))
 
         for name, task in self.log_tasks.items(): 
