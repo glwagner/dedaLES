f52286368d8abe9ce9e871fe917c945e1b0f4f07
Keaton J. Burns <keaton.burns@gmail.com>
2019-02-27 20:30:46 -0500

    Fill in more setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
-from setuptools import setup, find_packages
+import setuptools
+from setuptools import setup
+
+# Grab long_description from README
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(
     name="dedaLES",
-    packages=find_packages(),
+    author="Gregory L. Wagner",
+    author_email="wagner.greg@gmail.com",
+    description="A package for performing Direct Numerical Simulation (DNS) and Large Eddy Simulation (LES) of fluid flows using the Dedalus framework.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/glwagner/dedaLES",
+    classifiers=["Programming Language :: Python :: 3"],
+    packages=setuptools.find_packages(),
     use_scm_version=True,
     setup_requires=['setuptools_scm'])
 
