8eac1a87904ceaafdce67038b8e131ded6a02d7b
Gregory Wagner <wagner.greg@gmail.com>
2019-03-17 09:17:59 -0400

    Adds kburns as author to setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,14 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(
-    name="dedaLES",
-    author="Gregory L. Wagner",
-    author_email="wagner.greg@gmail.com",
-    description="A package for performing Direct Numerical Simulation (DNS) and Large Eddy Simulation (LES) of fluid flows using the Dedalus framework.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/glwagner/dedaLES",
-    classifiers=["Programming Language :: Python :: 3"],
-    packages=setuptools.find_packages(),
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'])
-
+    name = "dedaLES",
+    author = "Keaton Burns, Gregory L. Wagner",
+    author_email = "keaton.burns@gmail.com, wagner.greg@gmail.com",
+    description = "A package for performing Direct Numerical Simulation (DNS) and Large Eddy Simulation (LES) of fluid flows using the Dedalus framework.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/glwagner/dedaLES",
+    classifiers = ["Programming Language :: Python :: 3"],
+    packages = setuptools.find_packages(),
+    use_scm_version = True,
+    setup_requires = ['setuptools_scm'])
